ba02ce8f81acfa0f484563887facb8c2f4af2ac0
MarkOdhiambo <markokodeodhiambo@gmail.com>
2022-11-23 16:16:56 +0300

    Update to run on RaspberryPi for the inclusion of a test file

--- crystaltest.py
+++ crystaltest.py
@@ -76,15 +76,15 @@ if __name__=='__main__':
             while wake:
                 #Openning message event handler
                 if open==1:
-                    speak("What can I do for you set a remainder or access a todo list?")
+                    print("What can I do for you set a remainder or access a todo list?")
                 if open==0:
                     logManager("Wake word detected program begun")
                     print("Wake word detected.")
-                    speak(timeSalutation()+" This is the crystal time management platform I can manage a todo list and set a remainder. What would you like to do today?")
+                    print(timeSalutation()+" This is the crystal time management platform I can manage a todo list and set a remainder. What would you like to do today?")
                     open+=1
                 statement=takeUtterance(5).lower()
                 if len(statement)==4:
-                    speak("You haven't said a thing, how can I help you?")
+                    print("You haven't said a thing, how can I help you?")
                     continue
                 #This is the todo intent.
                 elif "todo" in statement or "list" in statement:
@@ -93,45 +93,45 @@ if __name__=='__main__':
                     #reinitializing statement
                     statement=""
                     while todoHook:
-                        speak("Would you like to add ,check or exit your todo list?")
+                        print("Would you like to add ,check or exit your todo list?")
                         todoTarget=takeUtterance(6).lower()
                         logManager("Accessed todo list")
                         if len(todoTarget)==4:
-                            speak("I didn't hear you could you repeat")
+                            print("I didn't hear you could you repeat")
                         elif "add" in todoTarget:
                             todoTarget=""
-                            speak("What would you like to add to the todo-list")
+                            print("What would you like to add to the todo-list")
                             add=takeUtterance(10).lower()
                             requests.post('http://127.0.0.1:5000/todos', data={'task': add}).json()
                             logManager("Add item to todolist")
                             #Variable to monitor your todo
                             hook=True
                             while hook:
-                                speak("Is there anything else you would like to add?")
+                                print("Is there anything else you would like to add?")
                                 addTodo=takeUtterance(10).lower()
                                 #Handling some error context
                                 if len(addTodo)==4:
                                     addTodo=""
-                                    speak("I didn't understand could you please repeat answer yes or no?")
+                                    print("I didn't understand could you please repeat answer yes or no?")
                                     continue
                                 elif 'yes' in addTodo:
                                     addTodo=""
-                                    speak("What is it?")
+                                    print("What is it?")
                                     addItem=takeUtterance(6).lower()
                                     requests.post('http://127.0.0.1:5000/todos', data={'task': addItem}).json()
                                     logManager("Add item to todolist")
                                 elif 'no' in addTodo:
                                     addTodo=""
-                                    speak("Okay thats great")
+                                    print("Okay thats great")
                                     logManager("Exit the todolist")
                                     hook=False
                                     todoHook=False
                         elif "check" in todoTarget:
                             todoTarget=""
-                            speak("This is your current todo-list")
+                            print("This is your current todo-list")
                             response=requests.get('http://127.0.0.1:5000/todos').json()
                             for todo in response:
-                                    speak(todo['task'])
+                                    print(todo['task'])
                         elif "exit" or "leave" in todoTarget:
                             todoTarget=""
                             todoHook=False
@@ -140,23 +140,23 @@ if __name__=='__main__':
                 #This is the remainder intent.
                 elif "remainder" in statement or "reminder" in statement:
                     logManager("Accessed the Remainder")
-                    speak("Would you like to set a remainder or check you remainder?")
+                    print("Would you like to set a remainder or check you remainder?")
                     remainder=takeUtterance(6).lower()
                     if len(remainder) ==4:
-                        speak("You can either set or check you remainders")
+                        print("You can either set or check you remainders")
                     elif "set" in remainder:
-                        speak("What day would you like to set your remainder")
+                        print("What day would you like to set your remainder")
                         #Remainder event handler
                         remain=True
                         while remain:
                             day=takeUtterance(6).lower()
                             senddate=speechtodate(day)
                             if senddate!=0:
-                                speak("What would you like to do?")
+                                print("What would you like to do?")
                                 rem=takeUtterance(6).lower()
-                                speak("At what time?")
+                                print("At what time?")
                                 time=takeUtterance(6).lower()
-                                speak("Would you like to set another remainder or no to exit?")
+                                print("Would you like to set another remainder or no to exit?")
                                 nextrem=takeUtterance(6).lower()
                                 requests.post('http://127.0.0.1:5000/remainder', data={'remainder': rem,'date':senddate, 'time':time}).json()
                                 if "no" in nextrem:
@@ -164,21 +164,20 @@ if __name__=='__main__':
                                 elif "yes" in nextrem:
                                     continue
                             elif senddate==0 or senddate=="None":
-                                speak("I didn't get the date well, could you please repeat?")
+                                print("I didn't get the date well, could you please repeat?")
                             elif 'exit' in senddate:
                                 remain=False
-
 
                 #This is the sleep/ exit command of the program.
                 elif "good bye" in statement or "goodbye" in statement or "ok bye" in statement or "turn off" in statement or "quit" in statement:
                     logManager("Exit intent detected program closed.")
                     wake=False
                     open=0
-                    speak('Thank you and take care')
+                    print ('Thank you and take care')
                     break
                 #Error handler of the program
                 else:
-                    speak("What would you like to access?")
+                    print("What would you like to access?")
         else:
             print("No wake word detected.")
     
